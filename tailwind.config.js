f26d67c376349acbd3de133c0cf90aa7be0e2297
leecfryan <61502605+leecfryan@users.noreply.github.com>
2024-10-22 16:59:33 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',  // Adjust these paths based on your project structure
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
